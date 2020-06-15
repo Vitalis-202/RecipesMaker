46a8198af9a39430eb97e2bdaf0b8823be58d897
Pablo Perez Lee <pablo.perez.lee@gmail.com>
2020-06-14 20:22:21 -0400

    Agregue Array y funciones MakeRecipe y ViewRecipe

--- RecipesMaker.kt
+++ RecipesMaker.kt
@@ -10,14 +10,15 @@ fun main(args: Array<String>) {
     """.trimIndent()
     //Ejecución
 
+    val ingredientes = arrayListOf<String>("Agua","Leche","Carne","Verduras","Frutas","Cereal","Huevos","Aceite")
     do{
         println(menu)
         val response:Int? = Integer.valueOf(readLine())
 
         when(response) {
 
-            1 -> println("Has seleccionado la opción $response - Hacer una receta")
-            2 -> println("Has seleccionado la opción $response - Ver mis recetas")
+            1 -> makeRecipe()
+            2 -> viewRecipe()
             3 -> ""
             else -> println("Escoja una opción válida de 1 a 3")
 
@@ -25,4 +26,24 @@ fun main(args: Array<String>) {
 
     }while(response != 3)
     println("Muchas gracias por utilizar RecipesMaker")
+}
+
+fun makeRecipe(){
+    val menu2:String = """
+        Hacer receta
+        Selecciona por categoría el ingrediente que buscas
+        1. Agua
+        2. Leche
+        3. Carne
+        4. Verduras
+        5. Frutas
+    """.trimIndent()
+    println(menu2)
+}
+fun viewRecipe(){
+    val menu3:String = """
+        Ver mis recetas
+    """.trimIndent()
+    println(menu3)
+
 }
